6ce5f3372dfc3798eabe71921dff442fb7626d91
egormaks <egor.maksimenka@gmail.com>
2023-04-07 21:34:32 -0700

    Added some basic REST APIs to the backend

--- backend/app.py
+++ backend/app.py
@@ -1,30 +1,42 @@
 import csv
-import re
 import psycopg2
+from flask import Flask, jsonify, request, g
 
-conn = psycopg2.connect(
-    dbname="postgres",
-    user="postgres",
-    password="mysecretpassword",
-    host="localhost",
-    port=5433
-)
+app = Flask(__name__)
 
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = psycopg2.connect(
+            dbname="postgres",
+            user="postgres",
+            password="mysecretpassword",
+            host="localhost",
+            port=5433
+        )
+    return db
 
-cur = conn.cursor()
-query = """
-SELECT * FROM moonboard_routes LIMIT 10;
-"""
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+    
+@app.route('/moonboard_get_all', methods=['GET'])
+def get_all_moonboard_routes():
+    conn = get_db()
+    cur = conn.cursor()
+    
+    cur.execute("SELECT * FROM moonboard_routes")
+    routes = cur.fetchall()
+    return jsonify(routes)
 
-cur.execute(query)
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-		
-query = """
-SELECT count(*) FROM moonboard_routes;
-"""
-cur.execute(query)
-print("\n\n" + str(cur.fetchone()[0]) + " total rows in the moonboard_routes table.")
-cur.close()
-conn.close()
+@app.route('/moonboard_get_top_n', methods=['GET'])
+def get_n_moonboard_routes():
+    conn = get_db()
+    cur = conn.cursor()
+    
+    cur.execute('SELECT * FROM moonboard_routes LIMIT %s', (int(request.args.get('N')),))
+    routes = cur.fetchall()
+    return jsonify(routes)
+
